784c4f15a7d103cba8edde9f4a370779e6f865ed
Ashley Davis <ashley@codecapers.com.au>
2019-04-06 19:05:02 +1000

    Split non-expanded chart def out to live in the DFP library.

--- src/index.ts
+++ src/index.ts
@@ -57,101 +57,16 @@ export enum VerticalLabelPosition {
  * Defines the configuration of an axis label.
  */
 export interface IAxisLabelConfig {
+
     /**
      * The text for the label.
      */
-    text?: string;
+    text: string;
 
     /**
      * Position of the label.
      */
     position?: HorizontalLabelPosition | VerticalLabelPosition;
-}
-
-/**
- * Configures an axis of the chart.
- */
-export interface IAxisConfig {
-    /**
-     * Sets the type of the axis' data.
-     * Default: AxisType.Indexed ("indexed")
-     */
-    axisType?: AxisType;
-
-    /**
-     * Label for the axis.
-     */
-    label?: string | IAxisLabelConfig;
-}
-
-/**
- * Configures a Y axis of the chart.
- */
-export interface IYAxisConfig extends IAxisConfig {
-    /**
-     * The minimum value to render on the axis.
-     */
-    min?: number;
-
-    /**
-     * The maximum value to render on the axis.
-     */
-    max?: number;
-}
-
-/**
- * Configures the legend of the chart.
- */
-export interface ILegendConfig {
-
-    /**
-     * Set to true (default) to show the legend for the chart   .
-     */
-    show?: boolean;
-}
-
-/**
- * Defines the chart.
- */
-export interface IPlotConfig {
-
-    /**
-     * The type of chart to render.
-     * Default to "line".
-     */
-    chartType?: ChartType;
-
-    /**
-     * Width of the plot.
-     * Default to 800.
-     */
-    width?: number;
-
-    /**
-     * Height of the plot.
-     * Default to 600.
-     */
-    height?: number;
-
-    /**
-     * Configuration for the x axis.
-     */
-    x?: IAxisConfig;
-
-    /**
-     * Configuration for the y axis.
-     */
-    y?: IYAxisConfig;
-
-    /**
-     * Configuration for the second y axis.
-     */
-    y2?: IYAxisConfig;
-
-    /**
-     * Configures the chart's legend.
-     */
-    legend?: ILegendConfig;
 }
 
 /**
@@ -184,37 +99,18 @@ export interface ISingleAxisMap {
  * Relates a single Y axis to data series.
  */
 export interface ISingleYAxisMap extends ISingleAxisMap {
+
     /**
      * Configure a separate X axis for the y axis.
      */
-    x?: string | ISingleAxisMap;
-}
-
-/**
- * Maps the columns in a dataframe to an axis in the chart.
- */
-export interface IAxisMap {
-
-    /**
-     * The x axis for the chart.
-     */
-    x?: string | ISingleAxisMap;
-
-    /**
-     * The y axis for the chart.
-     */
-    y?: string | string[] | ISingleYAxisMap | ISingleYAxisMap[];
-
-    /**
-     * The optional  second y axis for the chart.
-     */
-    y2?: string | string[] | ISingleYAxisMap | ISingleYAxisMap[];
+    x?: ISingleAxisMap;
 }
 
 /**
  * Configures an axis of the chart.
  */
-export interface IExpandedAxisConfig {
+export interface IAxisConfig {
+
     /**
      * Sets the type of the axis' data.
      * Default: AxisType.Indexed ("indexed")
@@ -230,7 +126,8 @@ export interface IExpandedAxisConfig {
 /**
  * Configures a Y axis of the chart.
  */
-export interface IExpandedYAxisConfig extends IExpandedAxisConfig {
+export interface IYAxisConfig extends IAxisConfig {
+    
     /**
      * The minimum value to render on the axis.
      */
@@ -245,7 +142,7 @@ export interface IExpandedYAxisConfig extends IExpandedAxisConfig {
 /**
  * Configures the legend of the chart.
  */
-export interface IExpandedLegendConfig {
+export interface ILegendConfig {
 
     /**
      * Set to true (default) to show the legend for the chart   .
@@ -256,7 +153,7 @@ export interface IExpandedLegendConfig {
 /**
  * Defines the chart.
  */
-export interface IExpandedPlotConfig {
+export interface IPlotConfig {
 
     /**
      * The type of chart to render.
@@ -276,28 +173,28 @@ export interface IExpandedPlotConfig {
     /**
      * Configuration for the x axis.
      */
-    x: IExpandedAxisConfig;
+    x: IAxisConfig;
 
     /**
      * Configuration for the y axis.
      */
-    y: IExpandedYAxisConfig;
+    y: IYAxisConfig;
 
     /**
      * Configuration for the second y axis.
      */
-    y2: IExpandedYAxisConfig;
+    y2: IYAxisConfig;
 
     /**
      * Configure the chart's legend.
      */
-    legend: IExpandedLegendConfig;
+    legend: ILegendConfig;
 }
 
 /**
  * Maps the columns in a dataframe to axis in the chart.
  */
-export interface IExpandedAxisMap {
+export interface IAxisMap {
 
     /**
      * The default x axis for the chart.
@@ -329,10 +226,10 @@ export interface IChartDef {
     /**
      * Defines the look of the chart.
      */
-    plotConfig: IExpandedPlotConfig;
+    plotConfig: IPlotConfig;
 
     /**
      * Maps fields in the data to axis' on the chart.
      */
-    axisMap: IExpandedAxisMap;
+    axisMap: IAxisMap;
 }
